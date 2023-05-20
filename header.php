3378aeea24ded99619a920aa4c688e2828857e10
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 12:43:48 -0700

    Update header.php

--- header.php
+++ header.php
@@ -7,28 +7,33 @@ include "includes/config.php";
 
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: login.html");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?>
+    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>  
 <!doctype html>
- <link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/" />
-<link rel="stylesheet" type="text/css" href="files/1" />
-<link rel="stylesheet" type="text/css" href="files/css/" />
+ <meta name="referrer" content="no-referrer" />
+<link rel="stylesheet"  href="flayout/css/util.css" />
+ <link rel="stylesheet"   href="layout/css/bootstrap.min.css" />
+<link rel="stylesheet"    href="layout/css/all.min.css" />
+<link rel="stylesheet"    href="layout/css/flexslider5.css" />
+<link rel="stylesheet"    href="layout/css/main.css" />
 <link rel="stylesheet" type="text/css" href="files/css/flags.css" />
 
+<link rel="stylesheet"   href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+<script  src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 
-
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
-<script type="text/javascript" src="files/js/bootbox.min.js"></script>
+<script  src="https://code.jquery.com/jquery-3.6.4.mi.js"></script>
+<script type="text/javascript" src="js/jquery.datatables.min.js">
+</script>
+<script type="text/javascript" src="layout/js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="layout/js/bootbox.min.js"></script>
 <script type="text/javascript" src="files/js/clipboard.min.js"></script>
+
+<script type="text/javascript" src="files/js/jquery.js"></script>
 <script type="text/javascript" src="component/accounts.js"></script>
- <meta name="referrer" content="no-referrer" />
+            
+
+
+    </head>
             
      
 
