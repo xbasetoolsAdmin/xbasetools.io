2fca603320b1da3cddc20eb00a4552d20a03701f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 01:28:34 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,1 +1,70 @@
 
+<!doctype html>
+<html>
+ 
+<head>
+    <link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
+    <link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="files/js/jquery.js?1"></script>
+    <script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
+    <script type="text/javascript" src="files/js/sorttable.js"></script>
+    <script type="text/javascript" src="files/js/table-head.js?3334"></script>
+    <script type="text/javascript" src="files/js/bootbox.min.js"></script>
+    <script type="text/javascript" src="files/js/clipboard.min.js"></script>
+ 
+    <link rel="shortcut icon" href="files/img/favicon.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" />
+    <meta name="referrer" content="no-referrer" />
+ 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Xbaseleet</title>
+</head>
+<style>
+    #table {
+        .sortable
+    }
+ 
+    table th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after {
+        content: " \25BE"
+    }
+ 
+    .label-as-badge {
+        border-radius: 0.5em;
+    }
+ 
+    body {
+        padding-top: 50px;
+    }
+ 
+    table.floatThead-table {
+        border-top: none;
+        border-bottom: none;
+        background-color: #fff;
+    }
+ 
+    @media (min-width: 768px) {
+        .dropdown:hover .dropdown-menu {
+            display: block;
+        }
+    }
+ 
+    #mydiv {
+        height: 400px;
+        position: relative;
+    }
+ 
+    .ajax-loader {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        /* presto! */
+ 
+    }
+</style>
+<script type="text/javascript" src="component/accounts.js"></script>
+</script>
