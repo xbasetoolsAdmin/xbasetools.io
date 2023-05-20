a2916529207da3039127adf933f5144000e3e229
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 13:14:26 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,6 +1,5 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
  <?php include "header.php";?>
-	
   <style type="text/css">
 			.dropdown:hover>
             
@@ -36,33 +35,16 @@
 			    content: " \25BE" 
 			}
 		</style>
-
-
-
-
-
-
-
-
-
-
-
 <?php include "mainDiv.php";?>
  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+  <div class="modal-dialog" role="document"><div class="modal-content">
+      <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>  <h4 class="modal-title" id="myModalLabel"></h4></div>
+      <div class="modal-body" id="modelbody"></div>
+      <div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
 </div>
-<?php include "component/DPremium.php";?>
+<?php include "components/dpremium.php";?>
 </body>
 </html>
