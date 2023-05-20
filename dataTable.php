40e4a23b891da591cd5039f3b6fb81deb483ef03
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 08:17:07 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -66,11 +66,7 @@ function ajaxinfo() {
  
     ajaxinfo();
          pageDiv(7, 'Banks - JeruxShop', 'table.html', 1);
-						
 			$('#filterbutton').click(function () 
-	
-				{$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-
 function buythistool(id){
   bootbox.confirm("Are you sure?", 
 			
