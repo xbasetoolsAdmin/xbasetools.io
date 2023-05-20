1b211468618b75d3a67b18a24deed20ec043e38f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 02:26:04 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,63 +1,35 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
- 
 <head>
-    <link rel="shortcut icon" href="files/img/favicon.ico" />
-    <meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" />
-    <meta name="referrer" content="no-referrer" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <title>Xbaseleet</title>
-    
-    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width" />
+  <title>myTable</title>
+  <link rel="stylesheet" href="layout/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="layout/css/main.css" />
+  <link rel="stylesheet" href="layout/css/util.css" />
+  <link rel="stylesheet" href="layout/css/all.min.css" />
+
+  <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
 </head>
-<style>
-    #accounts {
-        .dataTable_info
-    }
- 
-    display td {
-    }
- 
-    .label-as-badge {
-        border-radius: 0.5em;
-    }
- 
-    body {
-        padding-top: 50px;
-    }
- 
-    table.floatThead-table {
-        border-top: none;
-        border-bottom: none;
-        background-color: #fff;
-    }
- 
-    @media (min-width: 768px) {
-        .dropdown:hover .dropdown-menu {
-            display: block;
-        }
-    }
- 
-    #mydiv {
-        height: 400px;
-        position: relative;
-    }
- 
-    .ajax-loader {
-        position: absolute;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        margin: auto;
-        /* presto! */
- 
-    }
-</style>
-<script type="text/javascript" src="component/accounts.js"></script>
+<body>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">myTable</a>
+    </div>
+  </nav>
 
 
 
 
+
+
+
+
+
+
+
+
+  <script src="layout/css/bootstrap.min.js"></script>
+  <script src="component/account.js"></script>
+</body>
+</html>
