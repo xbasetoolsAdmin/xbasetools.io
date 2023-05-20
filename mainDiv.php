bfcd19cfb4506930718a316d78153d5057e897d4
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 05:49:02 -0700

    Update mainDiv.php

--- mainDiv.php
+++ mainDiv.php
@@ -75,7 +75,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
         $('.dropdown').hover(function() {
             $('.dropdown-toggle', this).trigger('click');
         });
-        pageDiv(7, 'Premium/Dating/Shopping - JeruxShop', 'premium.html', 1);
+        pageDiv(7, 'Premium/Dating/Shopping - JeruxShop', 'table.html', 1);
         var clipboard = new Clipboard('.copyit');
         clipboard.on('success', function(e) {
             setTooltip(e.trigger, 'Copied!');
