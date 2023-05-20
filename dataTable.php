274c922d62c228a3263935474b02e8f831eedaea
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 03:50:42 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -15,11 +15,80 @@
 
   <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Xbaseleet</title>
 </head>
-
-
-<script>
+<style>
+    #table {
+        .sortable
+    }
+ 
+    table th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after {
+        content: " \25BE"
+    }
+ 
+    .label-as-badge {
+        border-radius: 0.5em;
+    }
+ 
+    body {
+        padding-top: 50px;
+    }
+ 
+    table.floatThead-table {
+        border-top: none;
+        border-bottom: none;
+        background-color: #fff;
+    }
+ 
+    @media (min-width: 768px) {
+        .dropdown:hover .dropdown-menu {
+            display: block;
+        }
+    }
+ 
+    #mydiv {
+        height: 400px;
+        position: relative;
+    }
+ 
+    .ajax-loader {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        /* presto! */
+ 
+    }
+</style>
+<script type="text/javascript">
+    function ajaxinfo() {
+        $.ajax({
+            type: 'GET',
+            url: 'ajaxinfo.html',
+            timeout: 10000,
+ 
+            success: function(data) {
+                if (data != '01') {
+                    var data = JSON.parse(data);
+                    for (var prop in data) {
+                        $("#" + prop).html(data[prop]).show();
+                    }
+                } else {
+                    window.location = "logout.html";
+                }
+            }
+        });
+ 
+    }
+    setInterval(function() {
+        ajaxinfo()
+    }, 3000);
+ 
+    ajaxinfo();
  
     $(document).keydown(function(event) {
         if (event.which == "17")
@@ -51,12 +120,12 @@
  
         }
         document.title = obj.Title;
-        $("#mainDiv.php").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
+        $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
         $.ajax({
             type: 'GET',
             url: 'divPage' + n + '.html',
             success: function(data) {
-                $("#mainDiv.php").html(data).show();
+                $("#mainDiv").html(data).show();
                 newTableObject = document.getElementById('table');
                 sorttable.makeSortable(newTableObject);
                 $(".sticky-header").floatThead({
@@ -78,11 +147,12 @@
  
     });
  
+ 
     $(window).on('load', function() {
         $('.dropdown').hover(function() {
             $('.dropdown-toggle', this).trigger('click');
         });
-        pageDiv(7, 'Main - FeluxShop', 'table.html', 1);
+        pageDiv(7, 'Premium/Dating/Shopping - Xbaseleet', '', 1);
         var clipboard = new Clipboard('.copyit');
         clipboard.on('success', function(e) {
             setTooltip(e.trigger, 'Copied!');
@@ -108,6 +178,13 @@
         }, 1000);
     }
 </script>
+
+</head>
+
+
+<script>
+ 
+
 <body>
   <nav class="navbar nabber-expand-sm navbar-light bg-light">
     <div class="container">
