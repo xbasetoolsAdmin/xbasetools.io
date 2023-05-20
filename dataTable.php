83399e9b1be7ef889aefab431bb4cf6fe8c01994
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 12:14:42 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,6 +1,7 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
- 
-      <style type="text/css">
+ <?php include "header.php";?>
+	
+  <style type="text/css">
 			.dropdown:hover>
             
             .dropdown-menu {
@@ -35,7 +36,7 @@
 			    content: " \25BE" 
 			}
 		</style>
-<?php include "header.php";?>
+
 
 
 
