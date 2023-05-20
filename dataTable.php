2922d8886889f51135d6b5ea349362a0a3235d83
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 03:06:51 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "header.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
