f86b1299518764bb3aceba0e723bb69d91f67c39
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 07:59:09 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -2,30 +2,21 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 <!doctype html>
 <html>
-<head>
-	<meta charset="utf-8">
-		 <meta name="referrer" content="no-referrer" />
-				<link rel="shortcut icon" href="files/img/favicon.ico" />
-<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
-<script type="text/javascript" src="files/js/bootbox.min.js"></script>
-<script type="text/javascript" src="files/js/clipboard.min.js"></script>
+<head>	<meta charset="utf-8"> <meta name="referrer" content="no-referrer" /><link rel="shortcut icon" href="files/img/favicon.ico" /><meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" /><link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+
 
 <title>JeruxShop</title>
 </head>
-
+<?php
 	
 	
-	<?php
-	include "mainDiv.php";
-	?>	
+	                              include "mainDiv.php";
+	
+	
+	
+	
+	
+?>	
 	
 	
 
@@ -49,6 +40,11 @@
   <script src="js/jquery.datatables.min.js"></script>
   <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
   <script src="layout/css/bootstrap.min.js"></script>
+ <script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
+ <script type="text/javascript" src="files/js/sorttable.js"></script>
+ <script type="text/javascript" src="files/js/table-head.js?3334"></script>
+ <script type="text/javascript" src="files/js/bootbox.min.js"></script>
+ <script type="text/javascript" src="files/js/clipboard.min.js"></script>	
 <!---  <script src="component/accounts.js"></script> --->
 
 <script type="text/javascript">
