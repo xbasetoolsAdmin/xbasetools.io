a7930dd2e93d048cf7b8444d091aa241fe109b05
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 09:01:25 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -1,32 +1,53 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 <!doctype html>
 <html>
-<head>	<meta charset="utf-8"> <meta name="referrer" content="no-referrer" /><link rel="shortcut icon" href="files/img/favicon.ico" /><meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" /><link rel="stylesheet" type="text/css" href="files/css/flags.css" />
-<title>JeruxShop</title>
-</head>
-<?php
-	
-	
-	                              include "mainDiv.php";
-	
-	
-	
-	
-	
-?>	
+<head>	<meta charset="utf-8"> 
+	<meta name="referrer" content="no-referrer" />
+	<link rel="shortcut icon" href="files/img/favicon.ico" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" />
+	<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
+	 <link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
+<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+<script type="text/javascript" src="files/js/jquery.js?1"></script>
+<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
+<script type="text/javascript" src="files/js/sorttable.js"></script>
+<script type="text/javascript" src="files/js/table-head.js?3334"></script>
+<script type="text/javascript" src="files/js/bootbox.min.js"></script>
+<script type="text/javascript" src="files/js/clipboard.min.js"></script>
 
+<link rel="shortcut icon" href="files/img/favicon.ico" />
+	<title>JeruxShop</title>
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <script src="js/jquery.datatables.min.js"></script>
   <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
   <script src="layout/css/bootstrap.min.js"></script>
-		
- <script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
- <script type="text/javascript" src="files/js/sorttable.js"></script>
- <script type="text/javascript" src="files/js/table-head.js?3334"></script>
- <script type="text/javascript" src="files/js/bootbox.min.js"></script>
- <script type="text/javascript" src="files/js/clipboard.min.js"></script>	
-  <script src="component/accounts.js"></script>
+	<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+	
+</head>
+<?php
+	             include "mainDiv.php";
+?>	
 
+		
+ <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 <script type="text/javascript">
 $('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 function buythistool(id){
