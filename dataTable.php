69128e49f744c32104bab4f9a1e115b897e77866
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 04:05:36 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -18,95 +18,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <title>Xbaseleet</title>
-    
 <script type="text/javascript">
-    $(document).keydown(function(event) {
-        if (event.which == "17")
-            cntrlIsPressed = true;
-    });
- 
-    $(document).keyup(function() {
-        cntrlIsPressed = false;
-    });
- 
-    var cntrlIsPressed = false;
- 
- 
-    function pageDiv(n, t, u, x) {
-        if (cntrlIsPressed) {
-            window.open(u, '_blank');
-            return false;
-        }
-        var obj = {
-            Title: t,
-            Url: u
-        };
-        if (("/" + obj.Url) != location.pathname) {
-            if (x != 1) {
-                history.pushState(obj, obj.Title, obj.Url);
-            } else {
-                history.replaceState(obj, obj.Title, obj.Url);
-            }
- 
-        }
-        document.title = obj.Title;
-        $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
-        $.ajax({
-            type: 'GET',
-            url: 'divPage' + n + '.html',
-            success: function(data) {
-                $("#mainDiv").html(data).show();
-                newTableObject = document.getElementById('table');
-                sorttable.makeSortable(newTableObject);
-                $(".sticky-header").floatThead({
-                    top: 60
-                });
-                if (x == 0) {
-                    ajaxinfo();
-                }
-            }
-        });
-        if (typeof stopCheckBTC === 'function') {
-            var a = stopCheckBTC();
-        }
- 
-    }
- 
-    $(window).on("popstate", function(e) {
-        location.replace(document.location);
- 
-    });
- 
- 
-    $(window).on('load', function() {
-        $('.dropdown').hover(function() {
-            $('.dropdown-toggle', this).trigger('click');
-        });
-        pageDiv(7, 'Premium/Dating/Shopping - Xbaseleet', '', 1);
-        var clipboard = new Clipboard('.copyit');
-        clipboard.on('success', function(e) {
-            setTooltip(e.trigger, 'Copied!');
-            hideTooltip(e.trigger);
-            e.clearSelection();
-        });
- 
-    });
- 
- 
-    function setTooltip(btn, message) {
-        console.log("hide-1");
-        $(btn).tooltip('hide')
-            .attr('data-original-title', message)
-            .tooltip('show');
-        console.log("show");
-    }
- 
-    function hideTooltip(btn) {
-        setTimeout(function() {
-            $(btn).tooltip('hide');
-            console.log("hide-2");
-        }, 1000);
-    }
+
 </script>
 
 </head>
@@ -119,12 +32,10 @@
       <a class="navbar-brand" href="#">xbasetools</a>
       </div>
   </nav>
-  
 <style>
   
   
 </style>
-
 <?php include"mainDiv.php">
 
 
@@ -136,39 +47,13 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <script src="js/jquery.datatables.min.js"></script>
   <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
   <script src="layout/css/bootstrap.min.js"></script>
   <script src="component/accounts.js"></script>
-  
   <script type=text/javascript">
-    function ajaxinfo() {
-        $.ajax({
-            type: 'GET',
-            url: 'ajaxinfo.html',
-            timeout: 10000,
- 
-            success: function(data) {
-                if (data != '01') {
-                    var data = JSON.parse(data);
-                    for (var prop in data) {
-                        $("#" + prop).html(data[prop]).show();
-                    }
-                } else {
-                    window.location = "logout.html";
-                }
-            }
-        });
- 
-    }
-    setInterval(function() {
-        ajaxinfo()
-    }, 3000);
- 
-    ajaxinfo();
-    
-    </script>
   
+  </script>
 </body>
 </html>
