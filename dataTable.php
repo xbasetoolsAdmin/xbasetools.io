529afc1dc592f9eea04a088ac9c0107172ddef2c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 09:16:11 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -63,7 +63,7 @@ table.floatThead-table {
     
 
 </style>
-<?php include "mainDiv";?>
+<?php include "mainDiv.php";?>
 
  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
