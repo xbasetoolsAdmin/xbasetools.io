9a5b15400c84381c5b97fd3fceead3db9c4b45f5
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 06:51:42 -0700

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -141,7 +141,7 @@ $(window).on("popstate", function(e) {
 
 $(window).on('load', function() {
 $('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-   pageDiv(7,'Premium/Dating/Shopping - JeruxShop','premium.html',1);
+   pageDiv(7,'Premium/Dating/Shopping - JeruxShop','table.html',1);
    var clipboard = new Clipboard('.copyit');
     clipboard.on('success', function(e) {
       setTooltip(e.trigger, 'Copied!');
