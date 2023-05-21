b7bde7802787b2bd80eb6144db7839cafcfc98d2
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 08:24:18 -0700

    Update tutorial.php

--- tutorial.php
+++ tutorial.php
@@ -523,9 +523,137 @@
     <script type="text/javascript" src="components/main.js"></script>
     
 
+
+                </thead>
+            </table>
+        </div>
+    </div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <p class="heading" id="myModalHeader"></p>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">×</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="modelbody">
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    <div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
+ 
+            <div class="modal-content text-center">
+ 
+                <div class="modal-header d-flex justify-content-center">
+                    <p class="heading">Are you sure?</p>
+                </div>
+ 
+                <div class="modal-body">
+                    <i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
+                </div>
+ 
+                <div class="modal-footer flex-center">
+                    <a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
+                    <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
+                </div>
+            </div>
+ 
+        </div>
+    </div>
+ 
+ 
+    <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+ 
+            <div class="modal-content">
+ 
+                <div class="modal-body">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <img src="layout/images/balance.png">
+                        <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
+                        <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
+                            <i class="fas fa-book ml-1 white-text"></i>
+                        </a>
+                        <a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function() {
+        $('#dataTables').DataTable({
+                    "processing": true,
+                    "serverSide": true,
+                    "responsive": true,
+                    "scrollX": true,
+                    "order": [],
+                    "lengthMenu": [
+                        [10, 25, 50, 100, 500, 10000],
+                        [10, 25, 50, 100, 500, "All"]
+                    ],
+                    "columnDefs": [{
+                        "targets": [0],
+                        "visible": false
+                    }],
+ 
+        function buythistool(id) {
+            $('#modalConfirmBuy').modal('show');
+            webID = id;
+        }
+ 
+        function confirmbye(id) {
+            id = webID;
+            $.ajax({
+                method: "GET",
+                url: "buytool.php?id=" + id + "&t=accounts",
+                dataType: "text",
+                success: function(data) {
+                    if (data.match("buy")) {
+                        let lastid = data.split("buy,")[1];
+                        $("#premium" + id).html(`<button onclick=openitem(${lastid}) class="btn btn-success btn-sm" style="font-size: 11px; cursor:pointer">Order ${'#'+lastid}</button>`).show();
+ 
+                    } else {
+                        if (data.match("deleted")) {
+ 
+                            $("#premium" + id).html('Already sold / Deleted').show();
+ 
+ 
+                        } else {
+                            $('#modalCoupon').modal('show');
+                        }
+ 
+ 
+                    }
+                },
+            });
+        }
+ 
+        function openitem(order) {
+ 
+            $.ajax({
+                type: 'GET',
+                url: 'showOrder' + order,
+                success: function(data) {
+                    $("#myModalHeader").text('Order #' + order);
+                    $("#modelbody").append(data);
+                    $('#myModal').modal();
+ 
+ 
+                }
+            });
+ 
+        }
+    </script>
 </body>
  
 </html>
-            
             
          
