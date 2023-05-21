bf7f621d2dd4f9d42145b85f12a430aef52715f0
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 08:27:55 -0700

    Update tutorial.php

--- tutorial.php
+++ tutorial.php
@@ -587,9 +587,56 @@
             </div>
         </div>
     </div>
-    <script>
+    <script type="text/javascript">
+        $(document).keydown(function(event) {
+            if (event.which == "17")
+                cntrlIsPressed = true;
+        });
+ 
+        $(document).keyup(function() {
+            cntrlIsPressed = false;
+        });
+ 
+        var cntrlIsPressed = false;
+ 
+ 
+        $(window).on("popstate", function(e) {
+            location.replace(document.location);
+ 
+        });
+ 
+ 
+        $(window).on('load', function() {
+            $('.dropdown').hover(function() {
+                $('.dropdown-toggle', this).trigger('click');
+            });
+            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            var clipboard = new Clipboard('.copyit');
+            clipboard.on('success', function(e) {
+                setTooltip(e.trigger, 'Copied!');
+                hideTooltip(e.trigger);
+                e.clearSelection();
+            });
+ 
+        });
+ 
+ 
+        function setTooltip(btn, message) {
+            //console.log("hide-1");
+            $(btn).tooltip('hide')
+                .attr('data-original-title', message)
+                .tooltip('show');
+            //console.log("show");
+        }
+ 
+        function hideTooltip(btn) {
+            setTimeout(function() {
+                $(btn).tooltip('hide'); /*console.log("hide-2");*/
+            }, 1000);
+        }
+    </script>
         $(document).ready(function() {
-        $('#dataTables').DataTable({
+        $('#datatables').DataTable({
                     "processing": true,
                     "serverSide": true,
                     "responsive": true,
