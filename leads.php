60c1fe4ad9b6e6a654f642b5c8f4e6d8ca14792b
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 10:13:41 -0700

    Update leads.php

--- leads.php
+++ leads.php
@@ -1,1 +1,60 @@
-<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
+<?php 
+ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); 
+?>
+
+<?ph
+
+   <div id="mainDiv">
+    
+</div>
+    <script type="text/javascript">
+        $(document).keydown(function(event) {
+            if (event.which == "17")
+                cntrlIsPressed = true;
+        });
+ 
+        $(document).keyup(function() {
+            cntrlIsPressed = false;
+        });
+ 
+        var cntrlIsPressed = false;
+ 
+ 
+        $(window).on("popstate", function(e) {
+            location.replace(document.location);
+ 
+        });
+ 
+ 
+        $(window).on('load', function() {
+            $('.dropdown').hover(function() {
+                $('.dropdown-toggle', this).trigger('click');
+            });
+            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            var clipboard = new Clipboard('.copyit');
+            clipboard.on('success', function(e) {
+                setTooltip(e.trigger, 'Copied!');
+                hideTooltip(e.trigger);
+                e.clearSelection();
+            });
+ 
+        });
+ 
+ 
+        function setTooltip(btn, message) {
+            //console.log("hide-1");
+            $(btn).tooltip('hide')
+                .attr('data-original-title', message)
+                .tooltip('show');
+            //console.log("show");
+        }
+ 
+        function hideTooltip(btn) {
+            setTimeout(function() {
+                $(btn).tooltip('hide'); /*console.log("hide-2");*/
+            }, 1000);
+        }
+    </script>
+</body>
+ 
+</html>
