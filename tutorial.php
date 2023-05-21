29114690d76f93ae3cef99b51b835a1b306e6a42
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 08:50:30 -0700

    Update tutorial.php

--- tutorial.php
+++ tutorial.php
@@ -610,7 +610,7 @@
             $('.dropdown').hover(function() {
                 $('.dropdown-toggle', this).trigger('click');
             });
-            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            pageDiv(9, 'xBasetools', '', 1);
             var clipboard = new Clipboard('.copyit');
             clipboard.on('success', function(e) {
                 setTooltip(e.trigger, 'Copied!');
