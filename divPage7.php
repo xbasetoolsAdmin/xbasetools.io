dfd4ddad4bfac81b01647cf8ef55b1502b5d0a29
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-20 17:06:07 -0700

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -18,22 +18,24 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
      <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
          <div class="col-sm-12 table-responsive" id="mainDiv">
              <table id="account_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
-                 <thead>
-                     <tr>
-                         <th scope="col"> </th>
-                         <th scope="col">ID</th>
-                         <th scope="col">Website</th>
-                         <!---<th scope="col">Site Name</th>- -->
-                         <th scope="col">Details</th>
-                         <th scope="col">Seller</th>
-                         <th scope="col">Price</th>
-                         <th scope="col">Created Date </th>
-                         <th scope="col">Buy</th>
-                     </tr>
-                 </thead>
-                <tbody>
-                <?php 
-	$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+<thead>
+    <tr>
+     <th scope="col" ></th>
+         <th scope="col" >ID</th>
+      <th scope="col" >Country</th>
+      <th scope="col">Website</th>
+      <th scope="col">Details</th>
+      <th scope="col">Seller</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+      <th scope="col">Buy</th>
+    </tr>
+</thead>
+  <tbody>
+
+ <?php
+include("cr.php");
+$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
  while($row = mysqli_fetch_assoc($q)){
 	 
 	 	 $countryfullname = $row['country'];
@@ -44,14 +46,13 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 			 $SellerNick = "seller".$rpw["id"]."";
      echo "
  <tr>     
-         <th>                      <th>
-    <td id='id'> ".($row['id'])." </td> 
-    <td id='country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".($row['country'])." </td>
-    <td id='sitename'> ".($row['sitename'])." </td> 
-	<td> ".($row['infos'])." </td>
-    <td id='seller'> ".($SellerNick)."</td>
-    <td id='country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".($row['id'])." </td>
-    <td> ".($row['price'])."</td>
+	    <td> ".$row['a']."</td>"
+	    <td> ".$row['id']."</td>"
+    <td id='country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+    <td id='sitename'> ".htmlspecialchars($row['sitename'])." </td> 
+	<td> ".htmlspecialchars($row['infos'])." </td>
+    <td id='seller'> ".htmlspecialchars($SellerNick)."</td>
+    <td> ".htmlspecialchars($row['price'])."</td>
 	    <td> ".$row['date']."</td>";
     echo '
     <td>
@@ -62,31 +63,9 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
  }
 
  ?>
- </tbody>
-              </table>
-         </div>
-     </div>
-     </div>
-     </div>
-     </div>
-     </div>
-     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-         <div class="modal-dialog" role="document">
-             <div class="modal-content">
-                 <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                     <h4 class="modal-title" id="myModalLabel"></h4>
-                 </div>
-                 <div class="modal-body" id="modelbody"></div>
-                 <div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                 </div>
-             </div>
-         </div>
-     </div>
-    <script>
-	function buythistool(id){
-  bootbox.confirm("Are you sure?",
-		  
-function(result) {
+<script type="text/javascript">
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
         if(result ==true){
       $.ajax({
      method:"GET",
@@ -103,7 +82,7 @@ function(result) {
        ;}
   });
 }
-
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 function openitem(order){
   $("#myModalLabel").text('Order #'+order);
   $('#myModal').modal('show');
@@ -116,5 +95,22 @@ function openitem(order){
     }});
 
 }
-</script>
 
+</script>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
