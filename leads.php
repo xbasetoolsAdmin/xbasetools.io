4eab4c89cabafe5c1e606bd017e3e85ee8d8c2fa
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 10:23:36 -0700

    Update leads.php

--- leads.php
+++ leads.php
@@ -2,8 +2,15 @@
 ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); 
 ?>
 
-<?ph
+<?php 
 
+include "header.php";
+
+
+
+
+?>
+<?php include "navbar_header.php";?>
    <div id="mainDiv">
     
 </div>
