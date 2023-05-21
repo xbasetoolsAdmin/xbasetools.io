06af02f31de7d41d66e9e08165c9f873177fe2e0
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 08:18:09 -0700

    Update tutorial.php

--- tutorial.php
+++ tutorial.php
@@ -520,54 +520,9 @@
     <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
         <div class="col-sm-12 table-responsive" id="mainDiv">
             <table id="datatables" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);"></div>
-    <script type="text/javascript">
-        $(document).keydown(function(event) {
-            if (event.which == "17")
-                cntrlIsPressed = true;
-        });
- 
-        $(document).keyup(function() {
-            cntrlIsPressed = false;
-        });
- 
-        var cntrlIsPressed = false;
- 
- 
-        $(window).on("popstate", function(e) {
-            location.replace(document.location);
- 
-        });
- 
- 
-        $(window).on('load', function() {
-            $('.dropdown').hover(function() {
-                $('.dropdown-toggle', this).trigger('click');
-            });
-            pageDiv(9, 'leads - Xbasetools', 'tutorials', 1);
-            var clipboard = new Clipboard('.copyit');
-            clipboard.on('success', function(e) {
-                setTooltip(e.trigger, 'Copied!');
-                hideTooltip(e.trigger);
-                e.clearSelection();
-            });
- 
-        });
- 
- 
-        function setTooltip(btn, message) {
-            //console.log("hide-1");
-            $(btn).tooltip('hide')
-                .attr('data-original-title', message)
-                .tooltip('show');
-            //console.log("show");
-        }
- 
-        function hideTooltip(btn) {
-            setTimeout(function() {
-                $(btn).tooltip('hide'); /*console.log("hide-2");*/
-            }, 1000);
-        }
-    </script>
+    <script type="text/javascript" src="components/main.js"></script>
+    
+
 </body>
  
 </html>
