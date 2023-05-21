18c0359812b174265702bf23554d8a85ce3c21ed
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 10:07:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
- <?php include "header.php";?>
+ <?php 
+
+
+include "header.php";
+
+
+?>
+		<div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+		</div>
