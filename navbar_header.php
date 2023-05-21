db113d58552b911584f8d08f75b55f7c9a7eb83c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 00:26:05 -0700

    Update navbar_header.php

--- navbar_header.php
+++ navbar_header.php
@@ -1,6 +1,140 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 
 
+<body class="them">
+<style>
+    .navbar-nav .dropdown-menu
+    {
+      margin:0 !important
+    }
+    .theme-light {
+  --color-primary: #0060df;
+  --color-secondary: #ffffff;
+   --color-secondary2: #ecf0f1;
+  --color-accent: #fd6f53;
+  --font-color: #000000;
+  --color-nav: #ffffff;
+  --color-dropdown: #ffffff;
+  --color-card: #ffffff;
+   --color-card2: #d1ecf1;
+  --color-info: #0c5460;
+  --color-backinfo: #d1ecf1;
+  --color-borderinfo: #bee5eb;
+
+}
+.theme-dark {
+  --color-primary: #17ed90;
+  --color-secondary: #353B50;
+  --color-secondary2: #353B50;
+  --color-accent: #12cdea;
+  --font-color: #ffffff;
+  --color-nav: #363947;
+  --color-dropdown: rgba(171, 205, 239, 0.3);
+  --color-card: #262A37;
+   --color-card2: #262A37;
+   --color-info: #4DD0E1;
+  --color-backinfo: #262A37;
+  --color-borderinfo: #262A37;
+}
+.them {
+
+  background: var(--color-secondary);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.them h1 {
+  color: var(--font-color);
+  font-family: sans-serif;
+}
+.card-body {
+     color: var(--font-color);
+    }
+.them button {
+  color: var(--font-color);
+  background-color: #ffffff;
+  padding: 10px 20px;
+  border: 0;
+  border-radius: 5px;
+}
+.navbar.navbar-light .navbar-toggler {
+    color: var(--font-color);
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 40px;
+  width: 40px;
+  left: 0px;
+  bottom: 4px;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  box-shadow: 0 0px 15px #2020203d;
+  background: white url('https://i.ibb.co/FxzBYR9/night.png');
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(24px);
+  -ms-transform: translateX(24px);
+  transform: translateX(24px);
+  background: white url('https://i.ibb.co/7JfqXxB/sunny.png');
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+  </style>
+
 <script>
 
         function setTheme(themeName) {
