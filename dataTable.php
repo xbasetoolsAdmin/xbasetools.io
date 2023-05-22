6b10504f29805b4008ed3eef3cac6a8f41cbbbd8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 23:12:24 +0100

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -641,9 +641,6 @@ function hideTooltip(btn) {
 </script>
  
     <div style="padding-left:20px; display:inline-block; margin-bottom:10px">
-        <span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
-        <span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
-    </div>
     <div id="mainDiv"></div>
     <script type="text/javascript">
         $(document).keydown(function(event) {
