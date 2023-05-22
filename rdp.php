50994ae9d9336ad412cf5fd0d53fe2232fba59f2
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 06:33:06 -0700

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -1,304 +1,72 @@
-<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> <?php include "header.php";?>
-l
+<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> 
 
 
 
-?>
 
 
-<style>
-    .display td {
-        background: var(--color-card);
-        color: var(--font-color);
+
+<?php include "header.php";?>
+
+
+
+
+
+
+
+
+
+
+
+<?php include"footer.php";?>
+
+
+    <script type="text/javascript">
+        $(document).keydown(function(event) {
+            if (event.which == "17")
+                cntrlIsPressed = true;
+        });
  
-    }
+        $(document).keyup(function() {
+            cntrlIsPressed = false;
+        });
  
-    .dataTables_paginate .paginate_button.disabled:active {
+        var cntrlIsPressed = false;
  
-        color: var(--font-color);
  
-    }
+        $(window).on("popstate", function(e) {
+            location.replace(document.location);
  
-    .alert-info {
-        color: var(--color-info);
-        background-color: var(--color-backinfo);
-        border-color: var(--color-borderinfo);
+        });
  
-    }
  
-    #rdp_data_filter {
-        color: var(--font-color);
-    }
+        $(window).on('load', function() {
+            $('.dropdown').hover(function() {
+                $('.dropdown-toggle', this).trigger('click');
+            });
+            pageDiv(1, 'RDPs - XbaseTools', 'RDPs ', 1);
+            var clipboard = new Clipboard('.copyit');
+            clipboard.on('success', function(e) {
+                setTooltip(e.trigger, 'Copied!');
+                hideTooltip(e.trigger);
+                e.clearSelection();
+            });
  
-    #rdp_data_length {
-        color: var(--font-color);
-    }
+        });
  
-    #rdp_data_paginate {
-        color: var(--font-color);
-    }
  
-    #rdp_data_info {
-        color: var(--font-color);
-    }
-</style>
- 
-<body class="them">
-    <style>
-        .navbar-nav .dropdown-menu {
-            margin: 0 !important
+        function setTooltip(btn, message) {
+            //console.log("hide-1");
+            $(btn).tooltip('hide')
+                .attr('data-original-title', message)
+                .tooltip('show');
+            //console.log("show");
         }
  
-        .theme-light {
-            --color-primary: #0060df;
-            --color-secondary: #ffffff;
-            --color-secondary2: #ecf0f1;
-            --color-accent: #fd6f53;
-            --font-color: #000000;
-            --color-nav: #ffffff;
-            --color-dropdown: #ffffff;
-            --color-card: #ffffff;
-            --color-card2: #d1ecf1;
-            --color-info: #0c5460;
-            --color-backinfo: #d1ecf1;
-            --color-borderinfo: #bee5eb;
- 
+        function hideTooltip(btn) {
+            setTimeout(function() {
+                $(btn).tooltip('hide'); /*console.log("hide-2");*/
+            }, 1000);
         }
- 
-        .theme-dark {
-            --color-primary: #17ed90;
-            --color-secondary: #353B50;
-            --color-secondary2: #353B50;
-            --color-accent: #12cdea;
-            --font-color: #ffffff;
-            --color-nav: #363947;
-            --color-dropdown: rgba(171, 205, 239, 0.3);
-            --color-card: #262A37;
-            --color-card2: #262A37;
-            --color-info: #4DD0E1;
-            --color-backinfo: #262A37;
-            --color-borderinfo: #262A37;
-        }
- 
-        .them {
- 
-            background: var(--color-secondary);
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
- 
-        .them h1 {
-            color: var(--font-color);
-            font-family: sans-serif;
-        }
- 
-        .card-body {
-            color: var(--font-color);
-        }
- 
-        .them button {
-            color: var(--font-color);
-            background-color: #ffffff;
-            padding: 10px 20px;
-            border: 0;
-            border-radius: 5px;
-        }
- 
-        .navbar.navbar-light .navbar-toggler {
-            color: var(--font-color);
-        }
- 
-        /* The switch - the box around the slider */
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-        }
- 
-        /* Hide default HTML checkbox */
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
- 
-        /* The slider */
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: 0.4s;
-            transition: 0.4s;
-        }
- 
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 40px;
-            width: 40px;
-            left: 0px;
-            bottom: 4px;
-            top: 0;
-            bottom: 0;
-            margin: auto 0;
-            -webkit-transition: 0.4s;
-            transition: 0.4s;
-            box-shadow: 0 0px 15px #2020203d;
-            background: white url('https://i.ibb.co/FxzBYR9/night.png');
-            background-repeat: no-repeat;
-            background-position: center;
-        }
- 
-        input:checked+.slider {
-            background-color: #2196f3;
-        }
- 
-        input:focus+.slider {
-            box-shadow: 0 0 1px #2196f3;
-        }
- 
-        input:checked+.slider:before {
-            -webkit-transform: translateX(24px);
-            -ms-transform: translateX(24px);
-            transform: translateX(24px);
-            background: white url('https://i.ibb.co/7JfqXxB/sunny.png');
-            background-repeat: no-repeat;
-            background-position: center;
-        }
- 
-        /* Rounded sliders */
-        .slider.round {
-            border-radius: 34px;
-        }
- 
-        .slider.round:before {
-            border-radius: 50%;
-        }
-    </style>
-    <script>
-        function setTheme(themeName) {
-            localStorage.setItem('theme', themeName);
-            document.documentElement.className = themeName;
-        }
- 
-        // function to toggle between light and dark theme
-        function toggleTheme() {
-            if (localStorage.getItem('theme') === 'theme-dark') {
-                setTheme('theme-light');
-            } else {
-                setTheme('theme-dark');
-            }
-        }
- 
-        // Immediately invoked function to set the theme on initial load
-        (function() {
-            if (localStorage.getItem('theme') === 'theme-dark') {
-                setTheme('theme-dark');
-                document.getElementById('slider').checked = false;
-            } else {
-                setTheme('theme-light');
-                document.getElementById('slider').checked = true;
-            }
-        })();
     </script>
-
-</table>
-<script>
-$(document).ready(function() {
-$("#mailer_data").DataTable( {
-"lengthMenu": [[10, 25, 100, 500, -1], [10, 25, 100, 500, "All"]],
-				'iDisplayLength': 1000,
-				"aaSorting": []
-			} );
-		} );
-		</script>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
- <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document"><div class="modal-content">
-      <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>  <h4 class="modal-title" id="myModalLabel"></h4></div>
-      <div class="modal-body" id="modelbody"></div>
-      <div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<?php
-
-
-include "components/dpremium.php";
-
-?>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
-<div class="modal-content">
-<div class="modal-header">
-<p class="heading" id="myModalHeader"></p>
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true" class="white-text">&times;</span>
-</button>
-</div>
-<div class="modal-body" id="modelbody">
-</div>
-<div class="modal-footer justify-content-center">
-<a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
-</div>
-</div>
-</div>
-</div>
-
-<div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
-
-<div class="modal-content text-center">
-
-<div class="modal-header d-flex justify-content-center">
-<p class="heading">Are you sure?</p>
-</div>
-
-<div class="modal-body">
-<i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
-</div>
-
-<div class="modal-footer flex-center">
-<a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
-<a type="button" class="btn btn-info" data-dismiss="modal">No</a>
-</div>
-</div>
-
-</div>
-</div>
-
-
-<div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
-
-<div class="modal-content">
-
-<div class="modal-body">
-<div class="row d-flex justify-content-center align-items-center">
-<img src="layout/images/balance.png">
-<span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
-<a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
-<i class="fas fa-book ml-1 white-text"></i>
-</a>
-<a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
-</div>
-</div>
-</div>
-
-</div>
-</div>
 </body>
 </html>
