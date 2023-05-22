e38ea61d366aa6cd0a4c0d02b11f2a4b12ef1381
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 00:59:26 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -86,76 +86,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 </style>
  
 <body style="padding-top: 70px; padding-bottom: 70px;">
- 
-    <nav class="navbar navbar-default navbar-fixed-top ">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                <div class="navbar-brand" onClick="location.href='index.html'" onMouseOver="this.style.cursor='pointer'"><b><span class="glyphicon glyphicon-fire"></span> Xbaseleet <small><span class="glyphicon glyphicon-refresh"></span></small></b></div>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="topFixedNavbar1">
-                <ul class="nav navbar-nav">
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hosts <span class="glyphicon glyphicon-chevron-down" id="alhosts"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="rdp.html" onclick="pageDiv(1,'RDP - Xbaseleet','rdp.html',0); return false;">RDPs <span class="label label-primary label-as-badge" id="rdp"></span></a></li>
-                            <li><a href="cPanel.html" onclick="pageDiv(2,'cPanel - Xbaseleet','cPanel.html',0); return false;">cPanels <span class="label label-primary label-as-badge" id="cpanel"></span></a></li>
-                            <li><a href="shell.html" onclick="pageDiv(3,'Shell - Xbaseleet','shell.html',0); return false;">Shells <span class="label label-primary label-as-badge" id="shell"></span></a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Send <span class="glyphicon glyphicon-chevron-down" id="mail"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="mailer.html" onclick="pageDiv(4,'PHP Mailer - Xbaseleet','mailer.html',0); return false;">Mailers <span class="label label-primary label-as-badge" id="mailer"></span></a></li>
-                            <li><a href="smtp.html" onclick="pageDiv(5,'SMTP - Xbaseleet','smtp.html',0); return false;">SMTPs <span class="label label-primary label-as-badge" id="smtp"></span></a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Leads <span class="glyphicon glyphicon-chevron-down" id="all_leads"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="leads.html" onclick="pageDiv(6,'Leads - Xbaseleet','leads.html',0); return false;">Leads <span class="label label-primary label-as-badge" id="leads"></span></a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Accounts <span class="glyphicon glyphicon-chevron-down" id="accounts"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="premium.html" onclick="pageDiv(7,'Premium/Dating/Shop - Xbaseleet','premium.html',0); return false;">Premium/Dating/Shop <span class="label label-primary label-as-badge" id="premium"></span></a></li>
-                            <li><a href="banks.html" onclick="pageDiv(8,'Banks - Xbaseleet','banks.html',0); return false;">Banks <span class="label label-primary label-as-badge" id="banks"></span></a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Others <span class="glyphicon glyphicon-chevron-down" id="accounts"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="scampage.html" onclick="pageDiv(9,'Scampages - Xbaseleet','scampage.html',0); return false;">Scampage <span class="label label-primary label-as-badge" id="scams"></span></a></li>
-                            <li><a href="tutorial.html" onclick="pageDiv(10,'Tutorials - Xbaseleet','tutorial.html',0); return false;">Tutorial <span class="label label-primary label-as-badge" id="tutorials"></span></a></li>
-                        </ul>
-                    </li>
- 
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
- 
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tickets <span id="alltickets">
-                            </span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="tickets.html" onclick="pageDiv(11,'Tickets - Xbaseleet','tickets.html',0); return false;">Tickets <span class="label label-info"><span id="tickets"></span></span> </span></a></li>
-                            <li><a href="reports.html" onclick="pageDiv(12,'Reports - Xbaseleet','reports.html',0); return false;">Reports <span class="label label-info"><span id="reports"></span></span> </span> </a></li>
- 
-                        </ul>
-                    </li>
- 
-                    <li><a href="addBalance.html" onclick="pageDiv(13,'Add Balance - Xbaseleet','addBalance.html',0); return false;"><span class="badge"><b><span id="balance"></span></b> <span class="glyphicon glyphicon-plus"></span><span> </a></li>
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Account <span class="glyphicon glyphicon-user"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="setting.html" onclick="pageDiv(14,'Setting - Xbaseleet','setting.html',0); return false;">Setting <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
-                            <li><a href="orders.html" onclick="pageDiv(15,'Orders - Xbaseleet','orders.html',0); return false;">My Orders <span class="glyphicon glyphicon-shopping-cart pull-right"></span></a></li>
-                            <li><a href="addBalance.html" onclick="pageDiv(13,'Add Balance - Xbaseleet','addBalance.html',0); return false;">Add Balance <span class="glyphicon glyphicon-usd pull-right"></span></a></li>
- 
-                            <li class="divider"></li>
-                            <li><a href="logout.html">Logout <span class="glyphicon glyphicon-off pull-right"></span></a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+    
     <?php include"navbar_header.php";?>
     <?php include"ajax/banks_data.php";?>
 
