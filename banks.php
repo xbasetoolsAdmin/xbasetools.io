9713500832e83029ddfea2d5874eb3bf74f2a18b
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 23:26:36 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -293,7 +293,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             $('.dropdown').hover(function() {
                 $('.dropdown-toggle', this).trigger('click');
             });
-            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            pageDiv(8, 'Banks - xbaseTools', 'banks', 1);
             var clipboard = new Clipboard('.copyit');
             clipboard.on('success', function(e) {
                 setTooltip(e.trigger, 'Copied!');
