8f021e1e521c8f69af7e2a4d167e507dd8be77c3
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 22:27:53 +0100

    Update dataTable.php

--- dataTable.php
+++ dataTable.php
@@ -533,7 +533,7 @@
             $('.dropdown').hover(function() {
                 $('.dropdown-toggle', this).trigger('click');
             });
-            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            pageDiv(7, 'Add Balance - OdinShop', 'table.html', 1);
             var clipboard = new Clipboard('.copyit');
             clipboard.on('success', function(e) {
                 setTooltip(e.trigger, 'Copied!');
