fb23bfbf1b9f1f563d105d8a07b1b0b1f450f8d1
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 18:52:31 +0100

    Update mailer_data.php

--- ajax/mailer_data.php
+++ ajax/mailer_data.php
@@ -1,65 +1,6 @@
 
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> 
 
-
-
- <style>
-
-    .display td {
-        background: var(--color-card);
-        color: var(--font-color);
- 
-    }
- 
-    .dataTables_wrapper .dataTables_paginate .paginate_button {
- 
-        color: var(--font-color);
- 
-    }
- 
-    #balance_data_paginate .paginate_button {
-        color: var(--font-color);
- 
-    }
- 
-    .alert-info {
-        color: var(--color-info);
-        background-color: var(--color-backinfo);
-        border-color: var(--color-borderinfo);
- 
-    }
- 
-    #balance_data_filter {
-        color: var(--font-color);
-    }
- 
-    #balance_data_length {
-        color: var(--font-color);
-    }
- 
-    #balance_data_paginate {
-        color: var(--font-color);
-    }
- 
-    #balance_data_info {
-        color: var(--font-color);
-    }
-</style>
-
- <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
-        <ul>
-            <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
-            <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
-            <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
-            <li>There is <b> 0 </b> Available.</li>
-        </ul>
-    </div>
-    <input type=hidden id="type" name="type" value="5" />
-    <div class="row m-3 pt-1" style="color: var(--font-color);">
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
-            <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
-        </div>
         <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
             <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
             <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
@@ -93,3 +34,6 @@
             </table>
         </div>
     </div>
+    
+    
+    
