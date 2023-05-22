17cfb88e4b85785cc096f65181a48b207a69cb7c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 21:59:07 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -82,7 +82,7 @@ include"ajax/rdps_data.php";
             $('.dropdown').hover(function() {
                 $('.dropdown-toggle', this).trigger('click');
             });
-            pageDiv(7, 'Add Balance - OdinShop', 'addBalance', 1);
+            pageDiv(7, 'Add Balance - OdinShop', '', 1);
             var clipboard = new Clipboard('.copyit');
             clipboard.on('success', function(e) {
                 setTooltip(e.trigger, 'Copied!');
