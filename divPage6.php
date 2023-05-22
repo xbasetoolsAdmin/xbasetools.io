0d07826df42eac56a962f39961fe8700df532fd8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-21 19:13:08 -0700

    Update divPage6.php

--- divPage6.php
+++ divPage6.php
@@ -3,7 +3,7 @@
 
  </div>
     <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-        <div class="col-sm-12 table-responsive" id="mainDiv">
+        < div class="col-sm-12 table-responsive" id="mainDiv">
             <table id="lead_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
                 <thead>
                     <tr>
@@ -13,7 +13,6 @@
                         <th data-priority="6">Description</th>
                         <th data-priority="7">Email N</th>
                         <th data-priority="8">Seller</th>
-                        <th data-priority="2">Proof</th>
                         <th data-priority="9">Price</th>
                         <th data-priority="10">Added on </th>
                         <th class="all">Buy</th>
