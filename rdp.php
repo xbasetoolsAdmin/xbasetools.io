0206397d6a2f855877ee92414bbb88d80595b9b7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 19:38:12 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -1,8 +1,6 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> 
-<?php include"header.php";?>
-
-
-
+<?php
+include"header.php";?>
 <style>
     .display td {
         background: var(--color-card);
@@ -39,5 +37,14 @@
         color: var(--font-color);
     }
 </style>
+
+
+<?php
+include"navbar_header.php";
+include"right_navbar_header.php";
+?>
+
+
+
  
 <body class="them">
