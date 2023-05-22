7d83966bc09f625262889fbb27bdf39e026eab2f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 20:05:48 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -1,41 +1,13 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> 
 <?php
-include"header.php";?>
+include"header.php";
+
+?>
 <style>
-    .display td {
-        background: var(--color-card);
-        color: var(--font-color);
- 
-    }
- 
-    .dataTables_paginate .paginate_button.disabled:active {
- 
-        color: var(--font-color);
- 
-    }
- 
-    .alert-info {
-        color: var(--color-info);
-        background-color: var(--color-backinfo);
-        border-color: var(--color-borderinfo);
- 
-    }
- 
-    #rdp_data_filter {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_length {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_paginate {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_info {
-       color: var(--font-color);
-  }
+
+.display
+ td{background:var(--color-card);color:var(--font-color)}.dataTables_paginate .paginate_button.disabled:active{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#rdp_data_filter{color:var(--font-color)}#rdp_data_length{color:var(--font-color)}#rdp_data_paginate{color:var(--font-color)}#rdp_data_info{color:var(--font-color);
+
 </style>
 <body class="them"> <?php 
 
@@ -46,8 +18,6 @@ include"header.php";?>
 
 
 include"ajax/rdp_data.php";
-
-
 
 
 
@@ -82,13 +52,15 @@ $(window).on("popstate", (function (o)
             }(o.trigger), o.clearSelection()
     }))
 }));
+</script> <?php 
 
-    </script>
-    
-    
-    <?php include"footer.php";?>
-    
-    
+
+
+include"footer.php";
+
+
+
+?>
 </body>
  
 </html>
