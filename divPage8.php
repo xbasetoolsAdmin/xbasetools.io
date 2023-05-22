01a4bdd4080571c85400fb32b36b1cd01a583fd7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 16:08:01 -0700

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -14,7 +14,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
  <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
         <div class="col-sm-12 table-responsive" id="mainDiv">
-            <table id="lead_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+            <table id="banks_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
                 <thead>
                     <tr>
         <th data-priority="1"></th>
