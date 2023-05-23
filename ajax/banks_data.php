c170edd7c89ed09ba1560337ad9387c8539e572e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 17:58:13 -0700

    Update banks_data.php

--- ajax/banks_data.php
+++ ajax/banks_data.php
@@ -1,5 +1,5 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: login.html");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?> 
-
+ 
 <ul class="nav nav-tabs">
   <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
 </ul>
