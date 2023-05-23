817b394b3fbde2333fdf7075d57bb34c344a1b99
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 03:21:43 +0100

    Update divPage6.php

--- divPage6.php
+++ divPage6.php
@@ -1,1 +1,36 @@
- men
+
+
+
+    <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+        <div class="col-sm-12 table-responsive id="mainDiv">
+            <table id="lead_item" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+                <thead>
+                    <tr>
+                        <th data-priority="1"></th>
+                        <th class="all">ID</th>
+                        <th data-priority="3">Country</th>
+                        <th data-priority="6">Description</th>
+                        <th data-priority="7">Email N</th>
+                        <th data-priority="8">Seller</th>
+                     <!---   <th data-priority="2">Proof</th>--->
+                        <th data-priority="9">Price</th>
+                        <th data-priority="10">Added on </th>
+                        <th class="all">Buy</th>
+                    </tr>
+                </thead>
+                <thody>
+                
+                
+                
+                
+            </tbody>
+            </table>
+            
+            
+            
+            
+        </div>
+    </div>
+    
+    
+    
