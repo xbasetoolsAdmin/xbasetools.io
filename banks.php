abf52aa22622b583f58381d2f7ea2816963f309e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 02:17:47 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -31,8 +31,7 @@ include"ajax/banks_data.php";
     </div>
   </div>
 </div>   
-<script type="text/javascript" src="component/main.js">
-</script>
+<script type="text/javascript" src="component/main.js"></script>
 <script type="text/javascript">
  $(document).ready(function() {
  $('#banks_item').DataTable({
@@ -40,88 +39,8 @@ include"ajax/banks_data.php";
  [10, 25, 100, 500, -1],
   [10, 25, 100, 500, "All"] ],
 'iDisplayLength': 1000,
-            "aaSorting": []
-                });
+ "aaSorting": []  });
             });
-         $(document).keydown(function(event) {
-            if (event.which == "17")
-                cntrlIsPressed = true;
-        });
- 
-        $(document).keyup(function() {
-            cntrlIsPressed = false;
-        });
- 
-        var cntrlIsPressed = false;
- 
- 
-        $(window).on("popstate", function(e) {
-            location.replace(document.location);
- 
-        });
- 
- 
-        $(window).on('load', function() {
-            $('.dropdown').hover(function() {
-                $('.dropdown-toggle', this).trigger('click');
-            });
-            pageDiv(8, 'Banks - xbaseTools', 'banks', 1);
-            var clipboard = new Clipboard('.copyit');
-            clipboard.on('success', function(e) {
-                setTooltip(e.trigger, 'Copied!');
-                hideTooltip(e.trigger);
-                e.clearSelection();
-            });
- 
-        });
- 
- 
-        function setTooltip(btn, message) {
-            //console.log("hide-1");
-            $(btn).tooltip('hide')
-                .attr('data-original-title', message)
-                .tooltip('show');
-            //console.log("show");
-        }
- 
-        function hideTooltip(btn) {
-            setTimeout(function() {
-                $(btn).tooltip('hide'); /*console.log("hide-2");*/
-            }, 1000);
-        }
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#bank_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#bank_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#bank_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="bank_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="bank_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="bank_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=banks",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#bank"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
 
     </script>
     
