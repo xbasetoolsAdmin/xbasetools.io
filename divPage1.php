4c02700232da8ff01e4f1d2b5e0618ab374d1de4
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 15:04:09 +0100

    Update divPage1.php

--- divPage1.php
+++ divPage1.php
@@ -12,119 +12,62 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
 
-    <div class="row m-2 pt-3" style="color: var(--font-color); background-color: var(--color-card);">
-         <div class="col-sm-12 table-responsive" id="mainDiv">
-             <table id="rdp_item" class="display responsive table-hover" style="width:100%" style="color: var(--font-color); background-color: var(--color-card);">
-                 <thead>
-                     <tr>
- <th class="all" >   </th>
- <th data-priority="1" >ID</th> 
- <th data-priority="3">Country</th>
- <th data-priority="15">State</th>
- <th data-priority="14">Windows</th>
- <th data-priority="6">Ram</th>
- <th data-priority="17">Access</th>
- <th data-priority="8">Detect Hosting</th>
- <th data-priority="11">Seller</th>
-<th data-priority="13">Price</th>
-<th data-priority="16">Added on</th>
-<th class="all">Buy</th>
-                     </tr>
-                 </thead>
-            <tbody><?php
-include("cr.php");
-$q = mysqli_query($dbcon,"SELECT * FROM rdps WHERE sold='0' ORDER BY RAND()")or die(mysql_error());					
-	while($row = mysqli_fetch_assoc($q)){
-	 	 $countryfullname = $row['country'];
-		 $code = array_search("$countryfullname", $countrycodes);
-		 $countrycode = strtolower($code);
-	 	 $tld = end(explode(".", parse_url($row['url'], PHP_URL_HOST))); 
-		 $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>    
- <td> ".htmlspecialchars($row['id'])."</td>
-    <td id='country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-    <td> ".htmlspecialchars($row['city'])." </td>
-    <td id='windows'> ".htmlspecialchars($row['windows'])." </td>
-    <td> ".htmlspecialchars($row['ram'])." </td>
-    <td id='access'> ".htmlspecialchars($row['access'])." </td>
-    <td id='hosting'> ".htmlspecialchars($row['hosting'])."</td>
-    <td id='seller'> ".htmlspecialchars($SellerNick)."</td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".htmlspecialchars($row['date'])."</td>
-    <td>";
-    echo '
-	<span id="rdp'.$row['id'].'" title="buy" type="cpanel"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
- }
+                      <?php
+                        include('cr.php'); 
 
-  ?>
-                    </tbody>
-               </table>
-         </div>
-     </div>
-<script type="text/javascript">
-            $(document).ready(function() {
-                $('#rdp_item').DataTable({
-                    "lengthMenu": [
-                        [10, 25, 100, 500, -1],
-                        [10, 25, 100, 500, "All"]
-                    ],
-                    'iDisplayLength': 1000,
-                    "aaSorting": []
-                });
-            });
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#rdp_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#rdp_windows").text().toLowerCase() );var ck3 = $.trim( $(this).find("#rdp_access").text().toLowerCase() );var ck4 = $.trim( $(this).find("#rdp_hosting").text().toLowerCase() );var ck5 = $.trim( $(this).find("#rdp_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="rdp_country"]').val().toLowerCase() );var val2 = $.trim( $('select[name="rdp_windows"]').val().toLowerCase() );var val3 = $.trim( $('select[name="rdp_access"]').val().toLowerCase() );var val4 = $.trim( $('input[name="rdp_hosting"]').val().toLowerCase() );var val5 = $.trim( $('select[name="rdp_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || (ck2 != val2 && val2 != '' ) || (ck3 != val3 && val3 != '' ) || ck4.indexOf(val4)==-1 || (ck5 != val5 && val5 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=rdps",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#rdp"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
-</script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
+                        $query = "SELECT * FROM accounts";
+                        $q = mysqli_query($db, $query);
+                      ?>
+                        <table class="table table-striped mt-4 ">
+                            <thead>
+                              <tr>
+                                  <th>id</th>
+                                  <th>Card Number</th>
+                                  <th>Last Name</th>
+                                  <th>State</th>
+                                  <th>ZIP</th>
+                                  <th>SSN/DL</th>
+                                  <th>DOB</th>
+                                  <th>Price</th>
+                                  <th>Edit</th>
+                                  <th>Delete</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                              if(mysqli_num_rows($q) > 0)
+                              {
+                                while($row = mysqli_fetch_assoc($q))
+                                {
+                                  ?>
+                                  <tr>
+                                
+                           
+                                    <td> 
+                                      <?php echo $row['id']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['country']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['infos']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['sitename']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['resseller']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['date']; ?> 
+                                    </td>
+                                    <td>
+                                      <?php echo $row['date']; ?> 
+                                    </td>
+                                    <td>
+                                        <form action="edit_post_two.php" method="post">
+                                            <input type="hidden" name="edit_post_two_id" value="<?php echo $row['id']; ?>">
+                                            
+  <!-- your content here... -->
 
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
