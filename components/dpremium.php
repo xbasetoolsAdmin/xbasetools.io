2142665cf6153685d45fe6738d46ef418076a92a
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-22 17:30:08 -0700

    Update dpremium.php

--- components/dpremium.php
+++ components/dpremium.php
@@ -1,51 +1,26 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 
-<script type="text/javascript">
-    $(document).keydown(function(event) {
-        if (event.which == "17")
-            cntrlIsPressed = true;
-    });
- 
-    $(document).keyup(function() {
-        cntrlIsPressed = false;
-    });
- 
-    var cntrlIsPressed = false;
- 
- 
-    $(window).on("popstate", function(e) {
-        location.replace(document.location);
- 
-    });
- 
- 
-    $(window).on('load', function() {
-        $('.dropdown').hover(function() {
-            $('.dropdown-toggle', this).trigger('click');
-        });
-        pageDiv(7, 'Premium - XbaseTools', 'premium.php', 1);
-        var clipboard = new Clipboard('.copyit');
-        clipboard.on('success', function(e) {
-            setTooltip(e.trigger, 'Copied!');
-            hideTooltip(e.trigger);
-            e.clearSelection();
-        });
- 
-    });
- 
- 
- 
-    function setTooltip(btn, message) {
-        //console.log("hide-1");
-        $(btn).tooltip('hide')
-            .attr('data-original-title', message)
-            .tooltip('show');
-        //console.log("show");
-    }
- 
-    function hideTooltip(btn) {
-        setTimeout(function() {
-            $(btn).tooltip('hide'); /*console.log("hide-2");*/
-        }, 1000);
-    }
-</script>
+}
+.display td {
+
+}
+
+.dataTables_wrapper .dataTables_ .paginate_button {
+}
+
+
+#banks_items_pagenate .pagenate_button {
+}
+.alart-info {
+
+
+#banks_items_filter {
+
+}
+#banks_items_length {
+}
+#banks_items_pagenate {
+}
+#banks_items_info {
+
+}
