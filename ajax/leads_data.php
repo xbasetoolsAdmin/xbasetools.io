f21ed61fae4ca36daba1ab95fbf57671b1ad2c9a
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 03:14:10 +0100

    Update leads_data.php

--- ajax/leads_data.php
+++ ajax/leads_data.php
@@ -56,3 +56,151 @@
         </div>
     </div>
 
+
+ 
+    <script>
+        $(document).ready(function() {
+            var webID;
+            load_data();
+ 
+            function load_data(myarray) {
+                $('#lead_data').DataTable({
+                    "processing": true,
+                    "serverSide": true,
+                    "responsive": true,
+                    "scrollX": true,
+                    "order": [],
+                    "lengthMenu": [
+                        [10, 25, 50, 100, 500, 10000],
+                        [10, 25, 50, 100, 500, "All"]
+                    ],
+                    "columnDefs": [{
+                        "targets": [0],
+                        "visible": false
+                    }],
+ 
+                    "ajax": {
+                        url: "divPage32.html",
+                        type: "POST",
+                        data: {
+                            data_filter: myarray,
+                            type: document.getElementById('type').value,
+                            draw: 'draw',
+                            row: 'start',
+                            rowperpage: 'length',
+                            columnIndex: 'order',
+                            columnName: 'columns',
+                            columnSortOrder: 'order',
+                            searchValue: 'search'
+                        }
+                    },
+                    "columns": [{
+                            "data": 0
+                        },
+                        {
+                            "data": 1
+                        },
+                        {
+                            "data": 2
+                        },
+                        {
+                            "data": 3
+                        },
+                        {
+                            "data": 4
+                        },
+                        {
+                            "data": 5
+                        },
+                        {
+                            "data": 6
+                        },
+                        {
+                            "data": 7
+                        },
+                        {
+                            "data": 8
+                        },
+                        {
+                            "data": 9
+                        }
+                    ],
+ 
+                    "pageLength": 500
+                });
+            }
+ 
+            $(document).on('change', '.form-control', function() {
+                $('#lead_data').DataTable().destroy();
+                var country = $('#country').val();
+                var description = $('#infos').val();
+                var seller1 = $('#seller').val();
+                $idseller = seller1.split("Seller");
+                var seller = $idseller[1];
+                var myarray = {};
+ 
+                myarray[0] = country;
+                myarray[1] = description;
+                myarray[2] = seller;
+ 
+                if (country != '' || description != '' || seller != '') {
+ 
+                    load_data(myarray);
+                } else {
+                    load_data();
+                }
+            });
+ 
+ 
+        });
+ 
+        function buythistool(id) {
+            $('#modalConfirmBuy').modal('show');
+            webID = id;
+        }
+ 
+        function confirmbye(id) {
+            id = webID;
+            $.ajax({
+                method: "GET",
+                url: "buytool.php?id=" + id + "&t=leads",
+                dataType: "text",
+                success: function(data) {
+                    if (data.match("buy")) {
+                        let lastid = data.split("buy,")[1];
+                        $("#lead" + id).html(`<button onclick=openitem(${lastid}) class="btn btn-success btn-sm">Order ${'#'+lastid}</button>`).show();
+ 
+                    } else {
+                        if (data.match("deleted")) {
+ 
+                            $("#lead" + id).html('Already sold / Deleted').show();
+ 
+                        } else {
+                            $('#modalCoupon').modal('show');
+                        }
+ 
+ 
+                    }
+                },
+            });
+        }
+ 
+        function openitem(order) {
+ 
+ 
+            $.ajax({
+                type: 'GET',
+                url: 'showOrder' + order,
+                success: function(data) {
+                    $("#myModalHeader").text('Order #' + order);
+                    $("#modelbody").append(data);
+                    $('#myModal').modal();
+ 
+ 
+                }
+            });
+ 
+        }
+    </script>
+</body>
+</html>
